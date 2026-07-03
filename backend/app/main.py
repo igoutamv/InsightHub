0cb6c18585645e74e43187d2606c3a34c7c1b5a9
Goutam Vishwakarma <90381313+igoutamv@users.noreply.github.com>
2026-07-04 00:10:41 +0530

    Update CORS origins for the application

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,8 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
            "http://localhost:5173",
-            "http://127.0.0.1:5173",
-            "https://insighthub-app.vercel.app",
+            "https://insighthubai.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
